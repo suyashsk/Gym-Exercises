c98c2dcd8e94f9b6b72d2a26f00635f42005f838
suyashsk <suyashkakade.sk03@gmail.com>
2024-06-15 21:53:11 +0530

    Gym Exercises

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+
+import { useState } from 'react'
+import './App.css'
+import { useEffect } from 'react';
+import axios from 'axios'
+
+function App() {
+  
+  const[data,setData] = useState([]);
+  const[inputData,setInputData] = useState("");
+  useEffect(()=>{
+
+    async function getDataa(){
+      const options = {
+        method: 'GET',
+        url: 'https://exercisedb.p.rapidapi.com/exercises',
+        params: {
+          limit: '20',
+          offset: '0'
+        },
+        headers: {
+          'x-rapidapi-key': 'f51d4f4046msh81e25e82e26b3a5p13c8fbjsnfe54366e3bcf',
+          'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+        }
+      };
+
+      const result = await axios.request(options)
+      console.log(result.data);
+      setData(result.data);
+    }
+    getDataa();
+  },[]);
+  console.log(data)
+
+  function handleInputData(){
+    let temp = data.filter((item)=>{
+        if(item.bodyPart === inputData.toLowerCase())
+          return item;
+        
+    })
+    setData(temp)
+    setInputData("")
+  }
+
+  return (
+    <>
+      <div className='main_container'>
+        <div className='title'>
+          <h2>Awesome Exercises You Should Know</h2>
+        </div>
+        <div className='searchbar'>
+          <input className='text_search' type='text' placeholder='Search with your target bodypart' value={inputData} onChange={(e)=>setInputData(e.currentTarget.value)} />
+          <button className='btn_search' onClick={handleInputData}>Search</button>
+        </div>
+
+        <div className='data_content'>
+          {
+            data.map((item,index)=>{
+              return(
+                <div className='exercise_data' key={index}>
+                  <div className='image'>
+                    <img src={item.gifUrl} alt='body_parts_inage'/>
+                  </div>
+                  <div>
+                    <p>{item.bodyPart}</p>
+                    <p>{item.target}</p>
+                    <div className='secondary'>
+                      {item.secondaryMuscles.map((ab,index)=>{
+                        return(
+                          <p className='se1'>{item.secondaryMuscles[index]}</p>
+                        )
+                      })}
+                    </div>
+                    
+                    <p>{item.name}</p>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
